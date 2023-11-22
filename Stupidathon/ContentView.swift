d01f882a71efef4af05587db8897abd19487d8ec
Eansee <koh_ean_see@s2023.ssts.edu.sg>
2023-11-22 15:43:27 +0800

    volume

--- Stupidathon/ContentView.swift
+++ Stupidathon/ContentView.swift
@@ -8,24 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-   
+    
     @AppStorage("isDarkMode") private
     var isDark = false
     var body: some View {
-    VStack {
-        Toggle("Turn on dark mode", isOn: $isDark)
-            .padding(.horizontal)
-            .onChange(of: isDark) { newValue in
-                print("Dark mode is \(newValue ? "on" : "off")")
+        NavigationView {
+            VStack {
+                Toggle("Turn on dark mode", isOn: $isDark)
+                    .padding(.horizontal)
+                    .onChange(of: isDark) { newValue in
+                        print("Dark mode is \(newValue ? "on" : "off")")
+                    }
+                
+                    .preferredColorScheme(isDark ? .dark: .light)
+                
+                
+                NavigationLink(destination: infoView()) {
+                    Text("volume tester")
+                        .padding()
+                }
             }
-        
-            .preferredColorScheme(isDark ? .dark: .light)
-        Spacer()
-        
-        }
-        .accentColor(.green)
-                   }
-               }
+                    }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Stupidathon/infoView.swift
+++ Stupidathon/infoView.swift
@@ -43,7 +43,7 @@ struct infoView: View {
                 
 
             }
-            .navigationTitle("testing")
+            .navigationTitle("volume tester")
             
             
             
