786ca96d7eb491024cb5abbc27d85b2d8d4cc983
Eansee <koh_ean_see@s2023.ssts.edu.sg>
2023-11-22 09:32:01 +0800

    Initial Commit

---
+++ Stupidathon/StupidathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  StupidathonApp.swift
+//  Stupidathon
+//
+//  Created by Koh Ean See on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct StupidathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
